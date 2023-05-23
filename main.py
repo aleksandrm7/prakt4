3ddedaa156c7dfdc90be29bc7da61ae8ef2c4dcd
Lex <mys.alex@yandex.ru>
2023-05-23 13:32:05 +0300

    Add new nice function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import random
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+def print_random_number_2():
+    random_number_2 = random.randint(1, 200)
+    print(random_number_2)
+
+print_random_number_2()
