394ee19d2c589e12a4d51ea24b8568d90081d1a0
aleksandrm7 <80441149+aleksandrm7@users.noreply.github.com>
2023-05-23 14:12:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,3 @@ def print_hi(name):
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-def print_random_number():
-    random_number = random.randint(1, 100)
-    print(random_number)
-
-print_random_number()
