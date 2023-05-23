80e169900c3b561c0fca8a33e0ec1a403d096616
Lex <mys.alex@yandex.ru>
2023-05-23 13:29:08 +0300

    Add new function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import random
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+def print_random_number():
+    random_number = random.randint(1, 100)
+    print(random_number)
+
+print_random_number()
